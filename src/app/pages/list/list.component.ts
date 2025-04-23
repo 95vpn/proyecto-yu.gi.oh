11298cb84c0a18900c7f9976521eade9489d84e7
Johnny Herrera <jmjaramillo95@gmail.com>
2025-04-22 22:03:46 -0500

    list

--- src/app/pages/list/list.component.ts
+++ src/app/pages/list/list.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { CardComponent } from "../../components/card/card.component";
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-list',
   standalone: true,
-  imports: [],
+  imports: [CardComponent,CommonModule],
   templateUrl: './list.component.html',
   styleUrl: './list.component.scss'
 })
