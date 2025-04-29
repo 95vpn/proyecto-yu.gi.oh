e7a4b51a13e170f29f280285149b654283372ed7
Johnny Herrera <jmjaramillo95@gmail.com>
2025-04-28 22:32:40 -0500

    detail

--- src/app/pages/detail/detail.component.ts
+++ src/app/pages/detail/detail.component.ts
@@ -1,20 +1,27 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { CardService } from '../../services/card.service';
+import { Observable, tap } from 'rxjs';
+import { Card } from '../../interfaces/card.interface';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-detail',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './detail.component.html',
   styleUrl: './detail.component.scss'
 })
 export class DetailComponent implements OnInit{
 
   id!: string;
-  constructor(private route: ActivatedRoute) {}
+  card$!: Observable<Card>
+
+  constructor(private route: ActivatedRoute, private cardService: CardService) {}
 
   ngOnInit(): void {
     this.id = this.route.snapshot.paramMap.get('id') || '';
     console.log(this.id);
+    this.card$ = this.cardService.getCard(this.id).pipe(tap(console.log));
   }
 }
